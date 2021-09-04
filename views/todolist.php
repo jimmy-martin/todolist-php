6af8c69be77f702db90d5ca73e7527a079837c88
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-04 10:00:00 +0200

    bootstrap and css custom

--- views/todolist.php
+++ views/todolist.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="inc/css/style.css">
     <title>TODO LIST</title>
 </head>
 
 <body>
-    <main class="container">
+    <div class="container">
         <div class="header">
             <h1 class="header_title">
                 TODO LIST
@@ -20,21 +21,19 @@
                 Voici une application toute simple permettant de répertorier vos todos, mais aussi d'en ajouter et d'en supprimer !
             </p>
         </div>
-        <div>
-            <form action="" method="post">
-                <input type="text" name="name" id="name" placeholder="Ajouter un nouveau todo">
-                <button type="submit">Ajouter</button>
-            </form>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Todo name..." aria-label="Todo name..." aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
         </div>
-        <ul>
+        <ul class="list-group">
             <?php foreach ($todolist as $todo) : ?>
-                <li>
+                <li class="list-group-item">
                     <?= $todo['name'] ?>
                 </li>
             <?php endforeach ?>
         </ul>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
